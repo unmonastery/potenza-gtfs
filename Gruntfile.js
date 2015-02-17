07b329b30efbf25a420628b6586fd65bba68f8c2
mstn <mstn@git.com>
2015-02-17 11:36:43 +0100

    minimal structure to work with gtfs-maker

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+var config = require('./package.json').config;
+// upload settings specific to Potenza
+var settings = require('./potenza.json');
+
+
+var gtfsMaker = require('gtfs-maker');
+var loadData = gtfsMaker.load;
+var saveDataAsCsv = gtfsMaker.saveAsCsv;
+
+
+module.exports = function(grunt){
+
+  // utility function to parse command line options
+  function fetchOptions(){
+    return {
+      include:grunt.option( "include" ).toString().split(',')
+    };
+  }
+
+  grunt.registerTask('cache', function(){
+
+     var done = this.async();
+
+     gtfsMaker.cache()
+      .catch(function(err){
+          console.log(err);
+      }).then(done);
+
+  });
+
+
+};
